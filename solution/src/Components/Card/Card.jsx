eee2e588df3114675ec2b7fd2cd37e0af83d9f7b
DMB29 <mbilaldogan90@gmail.com>
2022-03-27 09:50:14 +0300

    finish

--- solution/src/Components/Card/Card.jsx
+++ solution/src/Components/Card/Card.jsx
@@ -9,12 +9,13 @@ const Card = () => {
         <div className="card-area-container">
             <div className="bars"></div>
             <h1 className="language-title">Languages</h1>
-            <div className="cards-container"></div>
+            <div className="cards-container">
             {categories.map((categorie,index)=> {
                 return(
                     <Item card={categorie} key={index} />
                 )
             })}
+            </div>
         </div>
     )
 }
--- solution/src/Components/Header/Haeder.jsx
+++ solution/src/Components/Header/Haeder.jsx
@@ -4,7 +4,7 @@ import "./Header.css"
 const Header = () => {
     return (
         <div className="logo-container">
-            <img className="react-logo " src={logo} alt="" srcset="" />
+            <img className="react-logo " src={logo} alt="logo" srcset="" />
         </div>
     )
 }
